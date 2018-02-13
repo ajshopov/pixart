bfed38d787549576368b6d72a966faaa81cbb6ff
ajshopov <alexanderjshopov@gmail.com>
2018-02-13 14:38:11 +1100

    2. enter button on input box

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 $('button').click(function(){
-  console.log('clicked');
   var inputColour = $('input').val();
   $('.brush-box').css('backgroundColor', inputColour);
+});
+
+$('input').on('keyup', function(key){
+  if(key.keyCode == 13){
+    var inputColour = $('input').val();
+    $('.brush-box').css('backgroundColor', inputColour);
+  }
 });
