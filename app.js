f665d9a26486d6cb1d34fc76f446d4ad343d1ccc
ajshopov <alexanderjshopov@gmail.com>
2018-02-13 14:48:07 +1100

    3. append square divs

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ $('input').on('keyup', function(key){
     $('.brush-box').css('backgroundColor', inputColour);
   }
 });
+
+for (var i = 0; i < 100; i++) {
+  var newDivElem = $('<div>', {'class': 'square'});
+  $('.container').append(newDivElem);
+}
